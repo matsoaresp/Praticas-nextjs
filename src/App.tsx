6e3eee1e2b335f795cce36001353a2d55568098c
Mateus Prado <maateussp@gmail.com>
2026-01-19 10:16:25 -0300

    feat: novas praticas

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { Container } from './components/Container';
 import { Footer } from './components/Footer';
 import { Heading } from './components/Heading';
-import { LoginForm } from './components/LoginForm';
-import { CounterButton } from './components/Contador';
 import './styles/global.css';
 import './styles/themes.css';
+import { SelectOptions } from './components/SelectOptions';
 function App() {
  
 return (
   <div>
     <Heading/>
   <Container>
-  <CounterButton/>
+  <SelectOptions>
+
+  </SelectOptions>
   </Container>
   <Footer/>
 
