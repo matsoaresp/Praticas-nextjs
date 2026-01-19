fc799732c8053695fcdf6945e4b033a6b14b4bfc
Mateus Prado <maateussp@gmail.com>
2026-01-19 11:42:05 -0300

    feat: deletando todos os arquivos para começar do zero

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { Container } from './components/Container';
-import { Footer } from './components/Footer';
-import { Heading } from './components/Heading';
-import './styles/global.css';
-import './styles/themes.css';
-import { SelectOptions } from './components/SelectOptions';
+
 function App() {
  
 return (
   <div>
-    <Heading/>
-  <Container>
-  <SelectOptions>
-
-  </SelectOptions>
-  </Container>
-  <Footer/>
-
   
 </div>
 )
